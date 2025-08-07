bc9397b7c80ee657516c748cc6c67daefabc22a9
pooja waykar <waykarpooja2002@gmail.com>
2025-08-07 11:59:28 +0530

    WIP: saving changes before rebase

---
+++ BookSystem/src/main/java/org/elevateLab/Book.java
@@ -0,0 +1,40 @@
+package org.elevateLab;
+
+public class Book {
+    private int id;
+    private String title;
+    private String author;
+    private boolean isIssued;
+
+    public Book(int id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.isIssued = false;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean isIssued() {
+        return isIssued;
+    }
+
+    public void issue() {
+        this.isIssued = true;
+    }
+
+    public void returnBook() {
+        this.isIssued = false;
+    }
+
+    public String toString() {
+        return id + " \t  " + title + " \tby" + author + (isIssued ? " \t  [Issued]" : " \t [Available]");
+    }
+}
+
---
+++ BookSystem/src/main/java/org/elevateLab/User.java
@@ -0,0 +1,24 @@
+package org.elevateLab;
+
+
+public class User {
+    private int userId;
+    private String name;
+
+    public User(int userId, String name) {
+        this.userId = userId;
+        this.name = name;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String toString() {
+        return "User ID: " + userId + ", Name: " + name;
+    }
+}
